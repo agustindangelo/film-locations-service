4bf4f959fce6158d7a8b3112fdeafa93d918e90b
agustindangelo <dangeloagustinariel@gmail.com>
2025-04-24 19:44:54 -0300

    Database updates and BaseController

--- FilmLocations.Api/Data/DatabaseSeeder.cs
+++ FilmLocations.Api/Data/DatabaseSeeder.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using CsvHelper;
 using Microsoft.Data.Sqlite;
 using Films.Api.Models;
+using CsvHelper.Configuration;
 
 namespace Films.Api.Data;
 public static class DatabaseSeeder
@@ -10,65 +11,70 @@ public static class DatabaseSeeder
     {
         try
         {
-            // Create the film_locations table if it doesn't exist
             var createTableCmd = connection.CreateCommand();
             createTableCmd.CommandText = @"
                 CREATE TABLE IF NOT EXISTS Films (
                     Id TEXT PRIMARY KEY,
                     Title TEXT,
-                    Release_Year INTEGER,
+                    ReleaseYear INTEGER,
                     Locations TEXT,
-                    Fun_Facts TEXT,
-                    Production_Company TEXT,
+                    FunFacts TEXT,
+                    ProductionCompany TEXT,
                     Distributor TEXT,
                     Director TEXT,
                     Writer TEXT,
-                    Actor_1 TEXT,
-                    Actor_2 TEXT,
-                    Actor_3 TEXT,
+                    Actor1 TEXT,
+                    Actor2 TEXT,
+                    Actor3 TEXT,
                     Longitude REAL,
                     Latitude REAL,
-                    Data_Loaded_At TEXT
+                    DataLoadedAt TEXT
                 );";
             createTableCmd.ExecuteNonQuery();
 
-            // Read and seed data from the CSV file
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HeaderValidated = null,
+                MissingFieldFound = null
+            };
             using var reader = new StreamReader(csvFilePath);
-            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            using var csv = new CsvReader(reader, config);
 
-            var records = csv.GetRecords<Film>();
+            var records = csv.GetRecords<FilmLocation>()
+                .Where(f => !string.IsNullOrWhiteSpace(f.Locations))
+                .ToList();
 
             var insertCmd = connection.CreateCommand();
             insertCmd.CommandText = @"
                 INSERT INTO Films (
-                    Title, Release_Year, Locations, Fun_Facts,
-                    Production_Company, Distributor, Director, Writer,
-                    Actor_1, Actor_2, Actor_3,
-                    Longitude, Latitude, Data_Loaded_At)
+                    Id, Title, ReleaseYear, Locations, FunFacts,
+                    ProductionCompany, Distributor, Director, Writer,
+                    Actor1, Actor2, Actor3,
+                    Longitude, Latitude, DataLoadedAt)
                 VALUES (
-                    $Title, $Release_Year, $Locations, $Fun_Facts,
-                    $Production_Company, $Distributor, $Director, $Writer,
-                    $Actor_1, $Actor_2, $Actor_3,
-                    $Longitude, $Latitude, $Data_Loaded_At);";
+                    $Id, $Title, $ReleaseYear, $Locations, $FunFacts,
+                    $ProductionCompany, $Distributor, $Director, $Writer,
+                    $Actor1, $Actor2, $Actor3,
+                    $Longitude, $Latitude, $DataLoadedAt);";
 
             foreach (var record in records)
             {
                 insertCmd.Parameters.Clear();
                 insertCmd.Parameters.AddWithValue("$Id", Guid.NewGuid().ToString());
                 insertCmd.Parameters.AddWithValue("$Title", record.Title ?? (object)DBNull.Value);
-                insertCmd.Parameters.AddWithValue("$Release_Year", record.Release_Year ?? (object)DBNull.Value);
+                insertCmd.Parameters.AddWithValue("$ReleaseYear", record.ReleaseYear ?? (object)DBNull.Value);
                 insertCmd.Parameters.AddWithValue("$Locations", record.Locations ?? (object)DBNull.Value);
-                insertCmd.Parameters.AddWithValue("$Fun_Facts", record.Fun_Facts ?? (object)DBNull.Value);
-                insertCmd.Parameters.AddWithValue("$Production_Company", record.Production_Company ?? (object)DBNull.Value);
+                insertCmd.Parameters.AddWithValue("$FunFacts", record.FunFacts ?? (object)DBNull.Value);
+                insertCmd.Parameters.AddWithValue("$ProductionCompany", record.ProductionCompany ?? (object)DBNull.Value);
                 insertCmd.Parameters.AddWithValue("$Distributor", record.Distributor ?? (object)DBNull.Value);
                 insertCmd.Parameters.AddWithValue("$Director", record.Director ?? (object)DBNull.Value);
                 insertCmd.Parameters.AddWithValue("$Writer", record.Writer ?? (object)DBNull.Value);
-                insertCmd.Parameters.AddWithValue("$Actor_1", record.Actor_1 ?? (object)DBNull.Value);
-                insertCmd.Parameters.AddWithValue("$Actor_2", record.Actor_2 ?? (object)DBNull.Value);
-                insertCmd.Parameters.AddWithValue("$Actor_3", record.Actor_3 ?? (object)DBNull.Value);
+                insertCmd.Parameters.AddWithValue("$Actor1", record.Actor1 ?? (object)DBNull.Value);
+                insertCmd.Parameters.AddWithValue("$Actor2", record.Actor2 ?? (object)DBNull.Value);
+                insertCmd.Parameters.AddWithValue("$Actor3", record.Actor3 ?? (object)DBNull.Value);
                 insertCmd.Parameters.AddWithValue("$Longitude", record.Longitude ?? (object)DBNull.Value);
                 insertCmd.Parameters.AddWithValue("$Latitude", record.Latitude ?? (object)DBNull.Value);
-                insertCmd.Parameters.AddWithValue("$Data_Loaded_At", record.Data_Loaded_At ?? (object)DBNull.Value);
+                insertCmd.Parameters.AddWithValue("$DataLoadedAt", record.DataLoadedAt ?? (object)DBNull.Value);
                 insertCmd.ExecuteNonQuery();
             }
             Console.WriteLine("Database seeded successfully.");
